fcf8e789bc9d33545215c42ed8af8d7e712f6f17
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-10-21 20:57:36 +0700

    setup image banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,39 @@
 import React from 'react'
 import "./index.css"
+import { Carousel, } from "react-bootstrap"
 import ImageBanner from "../../assets/images/banner.png"
+import banner1 from "../../assets/images/banner1.jpg"
+import banner2 from "../../assets/images/banner2.jpg"
 
 const index = () => {
     return (
         <div className="banner container">
-            <img src={ImageBanner} alt="banner" />
+            <Carousel>
+                <Carousel.Item interval={1000}>
+                    <img
+                        className="d-block w-100"
+                        src={ImageBanner}
+                        alt="First slide"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item interval={500}>
+                    <img
+                        className="d-block w-100"
+                        src={banner1}
+                        alt="Second slide"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={banner2}
+                        alt="Third slide"
+                    />
+
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
