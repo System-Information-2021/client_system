f0bd074f6bc91297a623803948751c82b2a2a645
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2021-10-24 19:16:11 +0700

    add api producy

--- src/container/AdminTemplate/ProductPage/index.js
+++ src/container/AdminTemplate/ProductPage/index.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom';
 import "./index.css"
 import Pagination from "react-pagination-library";
-
-
+import apiInstance from "../../../services/index"
 
 
 const Index = () => {
+    const [product, setProduct] = useState(null);
+    const [totalPage, setTotalPage] = useState(1);
     let history = useHistory()
     const [currentPage, setCurrentPage] = useState(1);
 
     const changeCurrentPage = (numPage) => {
         setCurrentPage(numPage);
     }
+
+    useEffect(() => {
+        async function fetchProduct() {
+            const { data } = await apiInstance({
+                url: `/product`,
+                method: "GET",
+                params: {
+                    page: currentPage
+                }
+            })
+            if (data.code === 200) {
+                setProduct(data.data)
+                setTotalPage(data.totalPage)
+            }
+        }
+        fetchProduct()
+    }, [totalPage, currentPage])
+
 
     return (
         <div className="admin_product">
@@ -21,8 +40,8 @@ const Index = () => {
                 <div className="admin_product_new" onClick={() => history.push("/admin/product/create")}>New Product</div>
             </div>
             <div className="admin_product_main_list">
-                <table class="table">
-                    <thead class="thead-dark">
+                <table className="table">
+                    <thead className="thead-dark">
                         <tr>
                             <th scope="col">Id</th>
                             <th scope="col">Product Name</th>
@@ -31,89 +50,38 @@ const Index = () => {
                             <th scope="col">Product Category</th>
                             <th scope="col">Quantity</th>
                             <th scope="col">Image</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Image</th>
                             <th scope="col">Active</th>
-
+                            <th scope="col">Description</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets co</td>
-                            <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets co</td>
-                            <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets co</td>
-                            <td>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets co</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets co</td>
+                        {
+                            product?.map((item) => {
+                                return (<tr>
+                                    <th scope="row">{item.id}</th>
+                                    <td>{item.name}</td>
+                                    <td>{item.price}</td>
+                                    <td>{item.id_brand}</td>
+                                    <td>{item.id_category}</td>
+                                    <td>{item.quantity}</td>
+                                    <td>
+                                        <div className="group_image">
+                                            <img src={`https://system-server-postgres.herokuapp.com/uploads/${item.image1}`} alt="iamge_product" />
+                                            <img src={`https://system-server-postgres.herokuapp.com/uploads/${item.image2}`} alt="iamge_product" />
+                                            <img src={`https://system-server-postgres.herokuapp.com/uploads/${item.image3}`} alt="iamge_product" />
+                                        </div>
+                                    </td>
+                                    <td>{item.active === true ? "True" : "False"}</td>
+                                    <td>{item.description}</td>
 
-                            <td>
-                                <div className="btn btn_edit" onClick={() => history.push("/admin/product/edit")}><ion-icon name="create-outline"></ion-icon></div>
-                                <div className="btn btn_delete"><ion-icon name="trash-outline"></ion-icon></div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>
-                                <div className="btn btn_edit"><ion-icon name="create-outline"></ion-icon></div>
-                                <div className="btn btn_delete"><ion-icon name="trash-outline"></ion-icon></div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>
-                                <div className="btn btn_edit"><ion-icon name="create-outline"></ion-icon></div>
-                                <div className="btn btn_delete"><ion-icon name="trash-outline"></ion-icon></div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>
-                                <div className="btn btn_edit"><ion-icon name="create-outline"></ion-icon></div>
-                                <div className="btn btn_delete"><ion-icon name="trash-outline"></ion-icon></div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>@twitter</td>
-                            <td>
-                                <div className="btn btn_edit"><ion-icon name="create-outline"></ion-icon></div>
-                                <div className="btn btn_delete"><ion-icon name="trash-outline"></ion-icon></div>
-                            </td>
-                        </tr>
+                                    <td>
+                                        <div className="btn btn_edit" onClick={() => history.push("/admin/product/edit")} style={{ marginRight: "10px" }}><ion-icon name="create-outline"></ion-icon></div>
+                                        <div className="btn btn_delete"><ion-icon name="trash-outline"></ion-icon></div>
+                                    </td>
+                                </tr>)
+                            })
+                        }
+
 
                     </tbody>
                 </table>
@@ -121,7 +89,7 @@ const Index = () => {
             <div className="product_pagination">
                 <Pagination
                     currentPage={currentPage}
-                    totalPages={10}
+                    totalPages={totalPage}
                     changeCurrentPage={changeCurrentPage}
                     theme="bottom-border"
                 />
