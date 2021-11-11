c7ad8144560123a61b82cb7cb9fbb02cd08bfc50
Thanh Linh <linh.thanh@wonderlabs.io>
2021-11-11 14:01:14 +0700

    handle function search

--- src/redux/Product/product.reducer.js
+++ src/redux/Product/product.reducer.js
@@ -1,7 +1,13 @@
-import { PRODUCT_SUCCESS, PRODUCT_ERROR, PAGE_PRODUCT } from "./product.types"
+import { PRODUCT_SUCCESS, PRODUCT_ERROR, PAGE_PRODUCT, BRAND_ERROR, BRAND_SUCCESS, CATEGORY_ERROR, CATEGORY_SUCCESS, SEARCH_ERROR, SEARCH_SUCCESS } from "./product.types"
 const INITIAL_STATE = {
     listProducts: [],
+    listBrands: [],
+    listCategorys: [],
+    listSearch: [],
     productError: null,
+    searchError: null,
+    brandError: null,
+    categoryError: null,
     totalPageProduct: 0
 }
 
@@ -19,6 +25,33 @@ const productReducer = (state = INITIAL_STATE, { type, payload }) => {
 
         case PAGE_PRODUCT:
             state.totalPageProduct = payload
+            return { ...state }
+
+        case BRAND_SUCCESS:
+            state.listBrands = payload
+            state.brandError = null
+            return { ...state }
+
+        case BRAND_ERROR:
+            state.brandError = payload
+            return { ...state }
+
+        case CATEGORY_SUCCESS:
+            state.listCategorys = payload
+            state.categoryError = null
+            return { ...state }
+
+        case CATEGORY_ERROR:
+            state.categoryError = payload
+            return { ...state }
+
+        case SEARCH_SUCCESS:
+            state.listSearch = payload
+            state.searchError = null
+            return { ...state }
+
+        case SEARCH_ERROR:
+            state.searchError = payload
             return { ...state }
 
         default:
